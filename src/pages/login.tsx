8c3c81768041f69a6b00955cba6f7fb2abecc979
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-04-25 21:22:25 +0200

    Modif page login et base

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,7 @@ import { CredentialResponse } from "../interfaces/google";
 
 // Todo: Update your Google Client ID here
 const GOOGLE_CLIENT_ID =
-  "1041339102270-jlljcjl19jo1hkgf695em3ibr7q2m734.apps.googleusercontent.com";
+  "981969793378-s30eh9a9m9emjk4buhiqcde9404obc91.apps.googleusercontent.com";
 
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
